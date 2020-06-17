591fa4f5619d6c2a4aafb666588fc59eabdd9923
Dušan Milojković <dmilojkovic76@gmail.com>
2020-06-17 08:37:05 +0200

    Pocetni inicijalni commit

---
+++ backend/src/index.ts
@@ -0,0 +1,12 @@
+import logger from './logger';
+import app from './app';
+
+const hostname = app.get('host');
+const port = app.get('port');
+const server = app.listen(port, hostname);
+
+process.on('unhandledRejection', (reason, p) =>
+  logger.error('Unhandled Rejection at: Promise ', p, reason)
+);
+
+server.on('listening', () => logger.info('Application started on http://%s:%d', hostname, port));
